8e7656bc88ad7dc0837c4be0d277a0cf395b438c
Bruno <brunoalnascimento@gmail.com>
2021-05-16 16:06:21 +0200

    add Pagination component

---
+++ components/Pagination/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { Box, Button, Text } from '@chakra-ui/react';
+
+type Props = {
+  totalPages: number;
+  currentPage: number;
+  sortBy: string;
+};
+
+export default function Pagination({ totalPages, currentPage, sortBy }: Props) {
+  const router = useRouter();
+
+  const handleNextPage = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+    router.push(`/ships/${sortBy}/${currentPage + 1}`);
+  };
+
+  const handlePrevPage = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+    router.push(`/ships/${sortBy}/${currentPage - 1}`);
+  };
+
+  return (
+    <Box mt="5" ml="5">
+      <Button disabled={currentPage === 1} mr="5" onClick={handlePrevPage}>
+        Prev
+      </Button>
+
+      <Button disabled={currentPage === totalPages} onClick={handleNextPage}>
+        Next
+      </Button>
+      <Text mt="2">
+        Page: {currentPage} / {totalPages}
+      </Text>
+    </Box>
+  );
+}
