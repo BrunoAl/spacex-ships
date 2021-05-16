6671576a1ab3215dc0f9a6907ad57dca16898b40
Bruno <brunoalnascimento@gmail.com>
2021-05-16 16:06:36 +0200

    add ShipDetails component

---
+++ components/ShipDetails/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Image, Box, Flex, Text, Center } from '@chakra-ui/react';
+import { IShip } from '../../interfaces';
+
+type Props = {
+  selectedShip: IShip;
+};
+
+export default function ShipDetails({ selectedShip: ship }: Props) {
+  if (!ship)
+    return (
+      <Flex bg="gray.700" justifyItems="center">
+        <Text m="0 auto" mt="10" color="white" fontSize="xl">
+          Select a ship to see the details!
+        </Text>
+      </Flex>
+    );
+
+  return (
+    <Flex bg="gray.700" flexDir="column" height="100vh">
+      <Image src={ship.image} alt={ship.name} fallbackSrc="https://via.placeholder.com/500" />
+      <Box mt="5" fontWeight="semibold" as="h2" lineHeight="tight" isTruncated pl="5" color="whitesmoke">
+        {ship.name}
+      </Box>
+      <Box color="white" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase" pl="5">
+        {ship.year_built}{' '}
+        {ship.roles.map(role => (
+          <span key={role}>&bull; {role}</span>
+        ))}
+      </Box>
+      <Box
+        color="white"
+        fontWeight="semibold"
+        letterSpacing="wide"
+        fontSize="xs"
+        textTransform="uppercase"
+        pl="5"
+        mt="10"
+      >
+        Launches: TODO
+      </Box>
+    </Flex>
+  );
+}
