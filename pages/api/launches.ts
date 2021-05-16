2b1fa8bdd0eec5c6cafad2c4447b3050b6678a8b
Bruno <brunoalnascimento@gmail.com>
2021-05-16 21:38:30 +0200

    update code comment

--- pages/api/launches.ts
+++ pages/api/launches.ts
@@ -1,7 +1,7 @@
 import nc from 'next-connect';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// I couldn't manage to query the original api to fetch launches by ids, so I created my own.
+// I couldn't manage to query the spaceX API to fetch launches by ids, so I created my own.
 const handler = nc<NextApiRequest, NextApiResponse>().get(async (req, res) => {
   const launchesIds = req.query['launches[]'];
 
