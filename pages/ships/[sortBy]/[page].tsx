a5b4d75a411fed37746e254edf22080de18d7265
Bruno <brunoalnascimento@gmail.com>
2021-05-16 16:10:24 +0200

    add ships page

---
+++ pages/ships/[sortBy]/[page].tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { GetStaticProps, GetStaticPaths } from 'next';
+import { addPagesToPaths } from '../../../utils';
+import { useRouter } from 'next/router';
+import { Grid, Container } from '@chakra-ui/react';
+import { IShip } from '../../../interfaces/index';
+import Ships from '../../../components/Ships';
+import Header from '../../../components/Header';
+import Pagination from '../../../components/Pagination';
+import ShipDetails from '../../../components/ShipDetails';
+
+const sortableValue = ['name', 'type', 'active', 'year_built', 'home_port'];
+// Not scalable! If new items get added to the api, it won't display all the pages in the pagination.
+const pages = ['1', '2', '3'];
+
+type Props = {
+  ships: IShip[];
+  page: number;
+  totalPages: number;
+  sortBy: string;
+};
+
+export default function ShipsPage({ ships, page, totalPages, sortBy }: Props) {
+  const [selectedShip, setSelectedShip] = React.useState<IShip | null>(null);
+
+  const router = useRouter();
+  const { query } = router;
+
+  if (typeof window !== 'undefined' && !query.isAuthenticated && !localStorage.getItem('bruno-spaceX-authenticated')) {
+    router.replace('/login');
+    return null;
+  }
+
+  return (
+    <div>
+      <Header />
+      <Grid templateColumns="repeat(2, 1fr)">
+        <div>
+          <Ships ships={ships} setSelectedShip={setSelectedShip} sortBy={sortBy} />
+          <Pagination currentPage={Number(page)} totalPages={totalPages} sortBy={sortBy} />
+        </div>
+        <ShipDetails selectedShip={selectedShip} />
+      </Grid>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: addPagesToPaths(
+      sortableValue.map(item => ({ params: { sortBy: item } })),
+      pages,
+    ),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  const { sortBy, page } = context.params;
+
+  const res = await fetch('https://api.spacexdata.com/v4/ships/query', {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+
+    body: JSON.stringify({
+      options: {
+        limit: 10,
+        page,
+        sort: sortBy,
+      },
+    }),
+  });
+  const ships = await res.json();
+
+  return {
+    props: {
+      ships: ships.docs,
+      totalPages: ships.totalPages,
+      page,
+      sortBy,
+    },
+  };
+};
