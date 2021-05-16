d8c05ebc07d775f95871c1bb14e01b3c7f24e48e
Bruno <brunoalnascimento@gmail.com>
2021-05-16 20:50:29 +0200

    add types to the addPagesToPaths args

--- utils/index.tsx
+++ utils/index.tsx
@@ -4,7 +4,7 @@ export function isEmailValid(email: string): boolean {
   return re.test(email);
 }
 
-export function addPagesToPaths(paths, pages) {
+export function addPagesToPaths(paths: { params: object }[], pages: string[]) {
   return pages.reduce((acc, page) => {
     return [...acc, ...paths.map(path => ({ ...path, params: { ...path.params, page } }))];
   }, []);
