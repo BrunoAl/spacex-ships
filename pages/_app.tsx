caff9f3b5ff086cae801564a5f32ae1a725e0fd3
Bruno <brunoalnascimento@gmail.com>
2021-05-16 17:57:17 +0200

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>SpaceX Ships</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />;
     </ChakraProvider>
   );
